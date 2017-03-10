4d39eda11d047c6c3738e6dac82b93bb204e106a
jaezen <jaezenlim@gmail.com>
2017-03-09 22:42:24 -0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/less/bootstrap.less';
+import { Navbar,Nav,NavItem, Jumbotron, Button } from 'react-bootstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React Master Lim</h2>
+        </div>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="http://google.com">Jaezen</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="#">Work</NavItem>
+            <NavItem eventKey={2} href="#">Services</NavItem>
+            <NavItem eventKey={1} href="#">Blog</NavItem>
+            <NavItem eventKey={2} href="#">Connect</NavItem>
+          </Nav>
+        </Navbar>
+    <Jumbotron>
+      <h1>I solve fuzzy tech marketing problems</h1>
+        <p>From YC startups to globally listed tech leaders</p>
+        <p><Button bsStyle="primary">Learn more</Button></p>
+    </Jumbotron>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Button bsStyle="primary" bsSize="large">Large button</Button>
+        <p>
+        Made by Jaezen
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
