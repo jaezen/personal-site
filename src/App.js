463c93e06e86d294996bd1fdcb5a119aafeca6e7
jaezen <jaezenlim@gmail.com>
2017-03-17 16:59:21 -0700

    Add images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import wallLean from './Images/wallLean.jpg';
+import profileShot from './Images/profileShot.jpeg';
 import './App.css';
 import 'bootstrap/less/bootstrap.less';
 import { Navbar,Nav,NavItem,Jumbotron,Button,Grid,Col,Row} from 'react-bootstrap';
@@ -14,7 +16,7 @@ class App extends Component {
         <Navbar className="Navbar">
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="http://google.com">Jaezen</a>
+              <a href="http://google.com">jaezen</a>
             </Navbar.Brand>
           </Navbar.Header>
           <Nav pullRight>
@@ -24,25 +26,24 @@ class App extends Component {
             <NavItem eventKey={2} href="#">Connect</NavItem>
           </Nav>
         </Navbar>
-      <Jumbotron>
+      <Jumbotron className="Jumbotron">
       <h1>I solve fuzzy tech marketing problems</h1>
         <p>From YC startups to globally listed tech leaders</p>
         &#9660;
       </Jumbotron>
+      <div className="heroImg">
+        <img src={wallLean} className="wallLean" alt="wallLean" />
+      </div>
       <Button bsStyle="primary" bsSize="large">Large button</Button>
       <Grid className="Grid">
         <Row className="Row-1">
-          <Col xs={12} md={8}><img src={images/iPhone.jpg} className="iPhone" alt="iPhone" /></Col>
+          <Col xs={6} md={4}>XXX</Col>
+          <Col xs={6} md={4}>XXX</Col>
           <Col xs={6} md={4}>XXX</Col>
         </Row>
         <Row className="Row-2">
-          <Col xs={6} md={4}>XXX</Col>
-          <Col xs={6} md={4}>XXX</Col>
-          <Col xs={6} md={4}>XXX</Col>
-        </Row>
-        <Row className="Row-3">
           <Col xs={6} md={4}>I just want to build what I see in my mind (Github)</Col>
-          <Col xs={6} md={4}>XXX</Col>
+          <Col xs={6} md={4}>XXX></Col>
         </Row>
       </Grid>
         <div className="Footer">
