a3ba643d965794e25f8ae5568f052ad1da43f275
jaezen <jaezenlim@gmail.com>
2017-03-16 18:29:08 -0700

    Attempt to add photo

--- src/App.js
+++ src/App.js
@@ -17,20 +17,13 @@ class App extends Component {
               <a href="http://google.com">Jaezen</a>
             </Navbar.Brand>
           </Navbar.Header>
-<<<<<<< HEAD
           <Nav pullRight>
-||||||| merged common ancestors
-          <Nav>
-=======
-          <Nav.Menu>
->>>>>>> 9230faead3730b3a326745c09bec4399bac32e55
             <NavItem eventKey={1} href="#">Work</NavItem>
             <NavItem eventKey={2} href="#">Services</NavItem>
             <NavItem eventKey={1} href="#">Blog</NavItem>
             <NavItem eventKey={2} href="#">Connect</NavItem>
-          </Nav.Menu>
+          </Nav>
         </Navbar>
-<<<<<<< HEAD
       <Jumbotron>
       <h1>I solve fuzzy tech marketing problems</h1>
         <p>From YC startups to globally listed tech leaders</p>
@@ -39,51 +32,23 @@ class App extends Component {
       <Button bsStyle="primary" bsSize="large">Large button</Button>
       <Grid className="Grid">
         <Row className="Row-1">
-          <Col xs={12} md={8}><code>&lt;{'Col xs={12} md={8}'} /&gt;</code></Col>
-          <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+          <Col xs={12} md={8}><img src={images/iPhone.jpg} className="iPhone" alt="iPhone" /></Col>
+          <Col xs={6} md={4}>XXX</Col>
         </Row>
         <Row className="Row-2">
-          <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
-          <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
-          <Col xsHidden md={4}><code>&lt;{'Col xsHidden md={4}'} /&gt;</code></Col>
+          <Col xs={6} md={4}>XXX</Col>
+          <Col xs={6} md={4}>XXX</Col>
+          <Col xs={6} md={4}>XXX</Col>
         </Row>
         <Row className="Row-3">
-          <Col md={6} mdPush={6}><code>&lt;{'Col md={6} mdPush={6}'} /&gt;</code></Col>
-          <Col md={6} mdPull={6}><code>&lt;{'Col md={6} mdPull={6}'} /&gt;</code></Col>
+          <Col xs={6} md={4}>I just want to build what I see in my mind (Github)</Col>
+          <Col xs={6} md={4}>XXX</Col>
         </Row>
       </Grid>
-
         <div className="Footer">
          <p>© 2017 Jaezen. All Rights Reserved.</p>
         </div>
 
-||||||| merged common ancestors
-    <Jumbotron>
-      <h1>I solve fuzzy tech marketing problems</h1>
-        <p>From YC startups to globally listed tech leaders</p>
-        <p><Button bsStyle="primary">Learn more</Button></p>
-    </Jumbotron>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button bsStyle="primary" bsSize="large">Large button</Button>
-        <p>
-        Made by Jaezen
-        </p>
-=======
-        <Jumbotron>
-          <h1>I solve fuzzy tech marketing problems</h1>
-            <p>From YC startups to globally listed tech leaders</p>
-            <p><Button bsStyle="primary">Learn more</Button></p>
-        </Jumbotron>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button bsStyle="primary" bsSize="large">Large button</Button>
-        <div className="Footer">
-          <p>© 2017 Jaezen. All Rights Reserved.</p>
-        </div>
->>>>>>> 9230faead3730b3a326745c09bec4399bac32e55
       </div>
     );
   }
